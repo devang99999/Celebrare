6204fb428212c80f8b919b745b8af8303da1cc0f
Devang <devanggandhi2222@gmail.com>
2022-01-21 18:05:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+  function myFunction1() {
+	  document.getElementById("swip").classList.toggle("swipsup");
+	//   document.getElementById("mn3").classList.toggle("cross3")
+	//   document.getElementById("menu").classList.toggle("menudisp")
+  }
+  function myFunction2() {
+	  document.getElementById("menu").classList.toggle("menu");
+      document.getElementById("mn1").classList.toggle("cross1");
+      document.getElementById("mn2").classList.toggle("cross2");
+      document.getElementById("mn3").classList.toggle("cross3");
+	//   document.getElementById("menu").classList.toggle("menudisp");
+  }
+  function myFunction3(){
+    document.getElementById("grab").classList.toggle("grab")
+  }
